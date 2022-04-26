a8f34e7b152deff82d4dd1b6530818da78084042
JasperVersantvoort <55187477+JasperVersantvoort@users.noreply.github.com>
2022-04-26 10:12:47 +0200

    blosum toegevoegd aan voorbeeld dataset

---
+++ OmschrijvenDataset.py
@@ -0,0 +1,12 @@
+import blosum as bl
+matrix = bl.BLOSUM(62)
+
+bestand = open("VoorbeeldDataset.csv")
+f = open("VoorbeeldOmgeschreven.csv", "w")
+f.write(bestand.readline().rstrip() + ",Blosum\n")
+for regel in bestand:
+    if "," in regel:
+        ref = regel.split(",")[1][0]
+        mut = regel.split(",")[1][-1]
+        score = matrix[ref + mut]
+        f.write(regel.rstrip() + "," + str(score) + "\n")
