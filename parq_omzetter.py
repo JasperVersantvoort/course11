78c45f45794acf314b8903dc8aac5716f09ab436
FemkeNijman <55234333+FemkeNijman@users.noreply.github.com>
2022-05-10 10:57:14 +0200

    Update parq_omzetter.py

--- parq_omzetter.py
+++ parq_omzetter.py
@@ -1,10 +1,14 @@
-import pandas as pd
+import pandas as pd, csv
 
 bestand = "train_data_bio_prodict.parq"
 
 def parq_lezer(bestand):
     text = pd.read_parquet(bestand, engine='pyarrow')
-    print(text)
+    #  print(text)
+
+    df = pd.DataFrame(text)
+    #print(df)
+    df.to_csv('test.csv')
 
 def main():
     parq_lezer(bestand)
