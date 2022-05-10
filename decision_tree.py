fa2ef023e01c8e7afe8e4987ae9680fb113fbcdb
FemkeNijman <55234333+FemkeNijman@users.noreply.github.com>
2022-05-10 11:36:50 +0200

    Create decision_tree.py

---
+++ decision_tree.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
+def decision_tree_maker(bestand):
+    col_names = ["mutation", "conservationPro", "conservationAla", "conservationHis",
+                 "conservationThr", "conservationGln", "conservationTyr", "conservationGly",
+                 "conservationArg", "conservationVal", "consWildType", "conservationGlu",
+                 "conservationMet", "conservationLys", "conservationIle", "conservationPhe",
+                 "conservationLeu", "conservationAsn", "conservationSer", "conservationAsp",
+                 "conservationCys", "consVariant", "conservationTrp", "source", "class"]
+
+    df = pd.read_csv('test.csv', header=0, low_memory=False, names=col_names)
+    print(df.head())
+
+def main():
+    bestand = 'test.csv.parq'
+    decision_tree_maker(bestand)
+main()
+
