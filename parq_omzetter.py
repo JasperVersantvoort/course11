1cd1d8545b5da805a904e2c93e5ce8d45c2ec6c4
FemkeNijman <55234333+FemkeNijman@users.noreply.github.com>
2022-05-10 10:22:20 +0200

    Update parq_omzetter.py

--- parq_omzetter.py
+++ parq_omzetter.py
@@ -3,7 +3,6 @@ import pandas as pd
 bestand = "train_data_bio_prodict.parq"
 
 def parq_lezer(bestand):
-    #problemen geeft
     text = pd.read_parquet(bestand, engine='pyarrow')
     print(text)
 
